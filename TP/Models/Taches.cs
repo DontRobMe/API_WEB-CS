f8c2eaf3c3a078471c2534605591443c89366ce3
francois gourbal <gourbal.francois@gmail.com>
2023-10-20 10:03:11 +0200

    1s

---
+++ TP/Models/Taches.cs
@@ -0,0 +1,14 @@
+﻿namespace TP1_Gestion_d_une_Liste_de_Taches.Models;
+
+public class Taches 
+{
+    public long id { get; set; }
+    public string Name { get; set; }
+    public bool Statut { get; set; }
+    public long UtilisateurId { get; set; }
+
+
+    public static long _nextTaskId = 1;
+    public static readonly List<Taches> _tasks = new List<Taches>();
+    
+}
